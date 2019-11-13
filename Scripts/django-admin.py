25c396b3e8823b310000e854b5374f0e2d0bf394
bradenb25 <bradenb25@gmail.com>
2019-11-12 19:09:15 -0700

    initeal commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\braden\documents\school\fall_2019\senior project part a\senior-project\meme-maker\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
