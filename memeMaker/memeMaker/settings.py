bd4a3999f9d0cd4e8f18fcd8152b2bae2992f4e6
bradenb25 <bradenb25@gmail.com>
2019-11-20 19:34:23 -0700

    Adding angular files

--- memeMaker/memeMaker/settings.py
+++ memeMaker/memeMaker/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT= os.path.join(BASE_DIR,'./static')
 
-STATICFILES_DIRS = [
-    # 'D:/home/site/wwwroot/memeMaker/memeMaker/static',
-    os.path.join(BASE_DIR, "./static")
-]
+# STATICFILES_DIRS = [
+#     # 'D:/home/site/wwwroot/memeMaker/memeMaker/static',
+#     os.path.join(BASE_DIR, "./static")
+# ]
